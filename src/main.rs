2a6094eb42e2651306bc87f034835dc17de255ea
J/A <archer884@gmail.com>
2015-05-19 21:07:04 -0400

    Fix fnmut problem
    Apparently, the issue was that the function itself was viewed as being
    "mutated," which makes sense if you think about the state of the random
    number generator as being part of the function itself. Pretty neat, to
    be honest.
    
    Anyway, what this means is that the function needs to be declared as
    mutable in the declaration for the consuming function.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 extern crate rand;
 use rand::distributions::{ IndependentSample, Range };
-use rand::{ Rand, Rng };
+use rand::{ Rand };
 
 fn main() {
     let mut rng = rand::weak_rng();
     let range = Range::new(1u32, 21u32);
-    let series = random_series(|| { range.ind_sample(&mut rng) })
+    let series = random_series(|| range.ind_sample(&mut rng))
         .take(10)
         .filter(|n| n & 1 == 0);
 
@@ -14,6 +14,6 @@ fn main() {
     }
 }
 
-fn random_series<'f, T: Rand, F: Fn() -> T + 'f>(f: F) -> Box<Iterator<Item=T> + 'f> {
+fn random_series<'f, T: Rand, F: FnMut() -> T + 'f>(mut f: F) -> Box<Iterator<Item=T> + 'f> {
     Box::new((0..).map(move |_| f()))
 }
